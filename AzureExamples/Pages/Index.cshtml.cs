ef68e9a85e40dceede667384629aa38a637a5930
wmizera96 <wmizera@outlook.com>
2022-02-26 14:23:25 +0100

    nested app settings

--- AzureExamples/Pages/Index.cshtml.cs
+++ AzureExamples/Pages/Index.cshtml.cs
@@ -5,10 +5,21 @@ namespace AzureExamples
 {
     public class IndexModel : PageModel
     {
-        public string AppSetting { get; set; }
+        public string SimpleSetting { get; set; }
+        public string NestedSetting { get; set; }
+
+        private readonly IConfiguration _configuration;
+        public IndexModel(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        
         public void OnGet()
         {
-            this.AppSetting = "setting";
+            this.SimpleSetting = this._configuration["appSetting"];
+            this.NestedSetting= this._configuration["nestedSetting:levelOne:levelTwo"];
+
         }
     }
 }
