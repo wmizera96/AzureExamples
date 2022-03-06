a01ecd928fd73c511f89bec78a7302cae8e0e9fa
wmizera96 <wmizera@outlook.com>
2022-03-07 00:01:39 +0100

    add user secrets and configuration

--- BlobStorageExample/Program.cs
+++ BlobStorageExample/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
 Console.WriteLine("Hello, World!");
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddUserSecrets<Program>()
+    .Build();
+
+
+Console.WriteLine($"[{config["ConnectionStrings:BlobStorage"]}]");
+
